fee52cfffeea27ac82f0c1370aeebe208c9c9548
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-09-30 10:34:50 -0300

    adiciona formulário e script receptor para a aula 03

---
+++ aula03/receptor.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+</head>
+
+<body>
+
+    <h1>Forms</h1>
+    <a href="/">Voltar para Home</a>
+    <pre>
+        <?php
+        print_r($_REQUEST);
+        ?>
+    </pre>
+
+
+
+</body>
+
+</html>
