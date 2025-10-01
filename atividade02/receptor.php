67533355f254fe5d263db4ee25f178eea067ea9e
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-10-01 08:22:54 -0300

    atualiza título da página e adiciona mensagem de confirmação no receptor.php; insere espaçamento no index.html

--- atividade02/receptor.php
+++ atividade02/receptor.php
@@ -4,10 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forms</title>
+    <title>Hospital Banana - Cadastro</title>
 </head>
 
 <body>
+
+    <br>
+    <br>
+    <br>
+
+    <center>
+        <img src="./public/hospital-banana-2.png" alt="Hospital Banana" width="200">
+
+        <h1>Cadastro de Usuário Confirmado com Sucesso</h1>
+        <hr>
+        <br>
+    </center>
 
     <h1>Forms</h1>
     <a href="/">Voltar para Home</a>
